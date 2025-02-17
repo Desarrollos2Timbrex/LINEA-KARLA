b962711ec402db0c208ec998eb257727db3fa2c0
AsosaESC <105983633+AsosaESC@users.noreply.github.com>
2025-02-17 17:21:04 -0600

    cambios

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Importa CommonModule
 
 @Component({
   selector: 'app-body',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './body.component.html',
-  styleUrl: './body.component.css'
+  styleUrls: ['./body.component.css']
 })
 export class BodyComponent {
+  //esto no se debe de hacer pero funciona por ahora
+  images = [
+    'assets/images/imagen1.jpg',
+    'assets/images/imagen2.jpg',
+    'assets/images/imagen3.jpg',
+    'assets/images/imagen4.jpg',
+    'assets/images/imagen5.jpg',
+    'assets/images/imagen6.jpg',
+    'assets/images/imagen7.jpg',
+    'assets/images/imagen8.jpg',
+    'assets/images/imagen9.jpg',
+    'assets/images/imagen10.jpg',
+    'assets/images/imagen11.jpg',
+    'assets/images/imagen12.jpg',
+    'assets/images/imagen13.jpg'
+  ];
 
+  @ViewChild('carousel', { static: false }) carousel!: ElementRef;
+
+  scrollAmount = 300; // Ajusta según el tamaño de cada imagen
+
+  moveCarousel(direction: 'next' | 'prev') {
+    if (this.carousel) {
+      const container = this.carousel.nativeElement;
+      if (direction === 'next') {
+        container.scrollLeft += this.scrollAmount;
+      } else {
+        container.scrollLeft -= this.scrollAmount;
+      }
+    }
+  }
 }
